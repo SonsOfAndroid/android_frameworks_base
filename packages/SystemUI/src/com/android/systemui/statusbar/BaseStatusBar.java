5259c2c5a987b6e842a2d19d8fe8ca4cc3f01742
Jens Doll <jens.doll@gmail.com>
2013-03-27 14:53:04 +0100

    Merge Navigation Bar and Pie Controls button config (1/2)

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -1353,15 +1353,17 @@ public abstract class BaseStatusBar extends SystemUI implements
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.PIE_CONTROLS), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.PIE_GRAVITY), false, this);
+                    Settings.System.PIE_POSITIONS), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.EXPANDED_DESKTOP_STATE), false, this);
         }
 
         @Override
         public void onChange(boolean selfChange) {
-            mPieTriggerSlots = Settings.System.getInt(mContext.getContentResolver(),
-                    Settings.System.PIE_GRAVITY, Position.BOTTOM.FLAG);
+            ContentResolver resolver = mContext.getContentResolver();
+
+            mPieTriggerSlots = Settings.System.getInt(resolver,
+                    Settings.System.PIE_POSITIONS, Position.BOTTOM.FLAG);
 
             attachPie();
         }
